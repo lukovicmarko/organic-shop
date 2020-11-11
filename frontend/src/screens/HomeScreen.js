309a3fdc265ef76edb2fe5ebd0dfaf46ba8f7a05
marko lukovic <lukovic86@gmail.com>
2020-11-11 22:59:10 +0100

    redux setup

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,19 +1,18 @@
 import React, { useState, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import Product from '../components/Product';
 import Header from '../components/Header';
-import axios from 'axios';
+import { listProducts } from '../actions/productActions';
 
 const HomeScreen = () => {
-    const [products, setProducts] = useState([]);
+    const dispatch = useDispatch();
+
+    const productsList = useSelector(state => state.products);
+    const { loading, error, products } = productsList;
 
     useEffect(() => {
-        const fetchProducts = async () => {
-            const { data } = await axios.get('/api/products');
-            setProducts(data);
-        }
-
-        fetchProducts();
-    }, []);
+        dispatch(listProducts());
+    }, [dispatch]);
 
     return (
         <div>
@@ -21,14 +20,16 @@ const HomeScreen = () => {
 
             <div className="products">
                 <h1 className="products-title">Latest Products</h1>
+                {
+                    loading ? <h2>Loading...</h2> : error ? <h3>error</h3> : <div className="products-list">
+                        {
+                            products.map(product => (
+                                <Product product={product} key={product._id} />
+                            ))
+                        }
+                    </div>
+                }
 
-                <div className="products-list">
-                    {
-                        products.map(product => (
-                            <Product product={product} key={product._id} />
-                        ))
-                    }
-                </div>
             </div>
 
         </div>
